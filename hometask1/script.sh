5613a1fee236958e43352bb99ff7b3ee4fa5a3eb
Sofia <sonyaporokhnavets@gmail.com>
2021-10-07 14:08:13 +0300

    first commit

---
+++ hometask1/script.sh
@@ -0,0 +1,6 @@
+sudo yum install -y  epel-release
+sudo yum install -y  nginx
+sudo systemctl start nginx
+sudo systemctl enable nginx
+
+sudo cp -f /vagrant/index.html /usr/share/nginx/html/index.html
